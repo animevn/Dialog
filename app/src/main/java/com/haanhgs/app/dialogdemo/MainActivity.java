90f2f59682605bd0abc7563ac22f3092173ff8bd
animevn <haanhth21@gmail.com>
2019-12-20 23:00:52 +0700

    add alert fragment

--- app/src/main/java/com/haanhgs/app/dialogdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/dialogdemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.haanhgs.app.dialogdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvMessage)
+    TextView tvMessage;
+    @BindView(R.id.bnClass)
+    Button bnClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.bnClass})
+    public void onViewClicked(View view) {
+
     }
 }
