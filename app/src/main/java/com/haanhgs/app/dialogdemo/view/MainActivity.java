be746f07d362187895c065af512814db342c6432
animevn <haanhth21@gmail.com>
2020-03-24 16:56:08 +0700

    test change git name

--- app/src/main/java/com/haanhgs/app/dialogdemo/view/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/dialogdemo/view/MainActivity.java
@@ -14,6 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+//test change git name
 public class MainActivity extends AppCompatActivity{
 
     @BindView(R.id.tvMessage)
